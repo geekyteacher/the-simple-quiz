30e289fc327c5588536e27847e338d0affbcce09
geekyteacher <17910442+geekyteacher@users.noreply.github.com>
2021-06-09 09:05:24 +0200

    added models

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    questionText = models.CharField(max_length = 240)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    choiceText = models.CharField(max_length = 140)
+    correct = models.BooleanField(default = False)
+
+    class Meta:
+        verbose_name = 'Questions'
+        verbose_name_plural = 'Questions'
--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Testing...1..2..3")
+
+def quizHome(request):
+    return HttpResponse("QUIZ --- Testing...1..2..3")
